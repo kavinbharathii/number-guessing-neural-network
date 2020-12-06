b922e11b40142260670e51e391c508438de4f16d
Kavin Bharathi <66783110+cipherDOT@users.noreply.github.com>
2020-12-06 16:39:14 +0530

    Add files via upload

---
+++ num_guesser.py
@@ -0,0 +1,67 @@
+import pygame
+# import math
+import numpy as np
+import tensorflow as tf
+from tensorflow import keras
+
+model = keras.models.load_model('[PATH TO YOUR MODEL HERE]')
+
+rez = 20
+width = 560
+height = 560
+display = pygame.display.set_mode((width, height))
+pygame.display.set_caption('Pixel Art')
+
+
+num_arr = np.zeros(shape=(28, 28), dtype=np.float)
+
+
+def get_rect_pos():
+    x = pygame.mouse.get_pos()[0]
+    y = pygame.mouse.get_pos()[1]
+    return (x - (x % rez), y - (y % rez), rez, rez)
+
+
+def get_grid_pos():
+    x = pygame.mouse.get_pos()[0]
+    y = pygame.mouse.get_pos()[1]
+    return x // rez, y // rez
+
+
+def draw_grid():
+    for i in range(1, int(width / rez)):
+        pygame.draw.line(display, (255, 255, 255),
+                         (i * rez, 0), (i * rez, width))
+
+    for j in range(1, int(height / rez)):
+        pygame.draw.line(display, (255, 255, 255),
+                         (0, j * rez), (height, j * rez))
+
+
+def main():
+    run = True
+    while run:
+        draw_grid()
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                run = False
+
+            if pygame.mouse.get_pressed()[0]:
+                pygame.draw.rect(display, (255, 255, 255), get_rect_pos())
+                num_arr[get_grid_pos()[1]][get_grid_pos()[0]] = 1
+
+            if event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_SPACE:
+                    num_arr.shape = (1, 784)
+                    prediction = model.predict(num_arr)
+                    print('\n')
+
+                    for index, result in enumerate(prediction[0]):
+                        print(
+                            f'label {index}: % confidence = {round(result * 100, 2)}%\n')
+
+        pygame.display.flip()
+
+
+if __name__ == '__main__':
+    main()
